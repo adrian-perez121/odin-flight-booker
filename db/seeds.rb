7536f37b1b08e7726bac96fdddc578053d1e393b
Adrian Perez <adrian1804perez@gmail.com>
2024-07-31 12:35:50 -0700

    Populate Airport model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+["CA", "NY", "AZ", "AL", "AK", "TX"].each do |code|
+  Airport.create(code: code)
+end

8af95bc8978b19f2bd69bc5979f163a97e825d6b
Adrian Perez <adrian1804perez@gmail.com>
2024-08-01 23:53:11 -0700

    Add index page with basic form functionality(no results shown yet)

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,7 @@
 class FlightsController < ApplicationController
+  def index
+    @departing_flights = Airport.joins(:departing_flights).distinct.map { |air_port| [air_port.code, air_port.id]}
+    @dates = Flight.distinct.pluck(:start_time).map { |d| [d.strftime("%m/%d/%Y"), d] }
+    @arriving_flights = Airport.joins(:arriving_flights).distinct.map { |air_port| [air_port.code, air_port.id]}
+  end
 end

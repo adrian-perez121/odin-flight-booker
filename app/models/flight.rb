fa123e5c09ff1ac0e98ffe40a5e0d621627e0c20
Adrian Perez <adrian1804perez@gmail.com>
2024-08-02 17:03:16 -0700

    Add bookings model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport"
   belongs_to :departure_airport, class_name: "Airport"
+  has_many :bookings
 
   scope :departing_from, -> (departure_id) { where(departure_airport_id: departure_id) }
   scope :arriving_at, -> (arrival_id) { where(arrival_airport_id: arrival_id)}

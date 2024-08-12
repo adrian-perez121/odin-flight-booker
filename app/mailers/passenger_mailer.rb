0da3348531c28ae096255f5f876e07be9d277cac
Adrian Perez <adrian1804perez@gmail.com>
2024-08-11 23:25:49 -0700

    Create Mailer Files

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,10 @@
 class PassengerMailer < ApplicationMailer
+  default from: "bookings@example.com"
+  def confirmation_booking
+    @passenger = params[:passenger]
+    @booking = @passenger.booking
+    @url = booking_url(@booking)
+
+    mail(to: @passenger.email, subject: "Flight Has Been Booked!")
+  end
 end
